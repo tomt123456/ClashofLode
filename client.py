dfed89d3ade453eac3dcb819f93bd8baf453c69d
tomt123456 <tomastrudic111@gmail.com>
2025-12-08 14:00:47 +0100

    pygame screen

--- client.py
+++ client.py
@@ -1,9 +1,19 @@
 import socket
+import pygame as pg
+
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_ip = input("Zadajte Server IP: ")
 port = 5678
+
+pg.init()
+clock = pg.time.Clock()
+screen = pg.display.set_mode((1280, 720))
+pg.display.set_caption("Clash of Loďe")
+
 try:
+    clock.tick(60)
     s.connect((server_ip, port))
     print("Pripojené")
     while True:
@@ -20,6 +30,10 @@ try:
         s.sendall(my_message.encode("utf-8"))
         if my_message.lower() == "exit":
             break
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
 except ConnectionRefusedError:
     print("Nepodarilo sa pripojiť na server.")
 finally:
--- server.py
+++ server.py
@@ -1,20 +1,29 @@
 import socket
+import pygame as pg
+
+
+# initialize pygame
+pg.init()
+clock = pg.time.Clock()
+screen = pg.display.set_mode((1280, 720))
+pg.display.set_caption("Clash of Loďe")
+
+running = True
+
 
 host = socket.gethostbyname(socket.gethostname())
 print(f"Server IP: {host}")
-
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 port = 5678
 s.bind((host, port))
-
 s.listen(1)
 print(f"Waiting for client on port {port}...")
-
 conn, addr = s.accept()
 print(f"Client connected: {addr}")
 
 try:
-    while True:
+    clock.tick(60)
+    while running:
         my_message = input("[Server] You: ")
         conn.sendall(my_message.encode("utf-8"))
 
@@ -27,6 +36,10 @@ try:
             break
 
         print(f"[Client]: {data.decode('utf-8')}")
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
 
 except KeyboardInterrupt:
     print("\nClosing connection...")

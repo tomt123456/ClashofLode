1bc1cbcbcef6e1778c63593d07a8a12f2df013df
MartinSeman <semanmartin802@gmail.com>
2026-01-13 17:12:43 +0100

    hosting layout

--- screens/hosting.py
+++ screens/hosting.py
@@ -9,9 +9,10 @@ class HostingScreen(ScreenBase):
         print("host init")
         super().__init__(app)
         self.host_ip_display = host_ip_display
+        self.title_font = pygame.font.Font(None, 60)
         self.img_background = pygame.image.load("assets/background1.png")
         self.img_background = pygame.transform.scale(self.img_background, (app.WIDTH, app.HEIGHT))
-        self.back_btn = Button(100, 500, 250, 100, "BACK")
+        self.back_btn = Button(app.WIDTH // 2 - 125, 500, 250, 100, "BACK")
         self.room_code = encode_ip(self.host_ip_display)
 
     def handle_event(self, event):
@@ -31,8 +32,8 @@ class HostingScreen(ScreenBase):
         surface.fill(Palette.C2)
         surface.blit(self.img_background, (0, 0))
 
-        msg1 = self.app.font.render("Waiting for player...", True, Palette.C8)
-        msg2 = self.app.font.render(f"Room Code: {self.room_code.upper()}", True, Palette.C8)
-        surface.blit(msg1, (50, 150))
-        surface.blit(msg2, (50, 200))
+        msg1 = self.app.title_font.render("Waiting for player...", True, Palette.C8)
+        msg2 = self.app.title_font.render(f"Room Code: {self.room_code.upper()}", True, Palette.C8)
+        surface.blit(msg1, (550, 150))
+        surface.blit(msg2, (550, 200))
         self.back_btn.draw(surface, self.app.font)
